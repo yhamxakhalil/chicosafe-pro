d6903dabe2e9dcb6e19a925f33ef89b37352a3e2
yhamxakhalil <yhamxakhalil@gmail.com>
2026-09-15 14:58:14 +0100

    Modify mail server settings for SSL support
    Updated mail server configuration to use SSL and changed port.

--- app.py
+++ app.py
@@ -18,9 +18,10 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', secrets.token_hex(32))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chicosafe.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['MAIL_SERVER']         = 'smtp.gmail.com'
-app.config['MAIL_PORT']           = 587
-app.config['MAIL_USE_TLS']        = True
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME']       = os.environ.get('MAIL_USERNAME', '')
 app.config['MAIL_PASSWORD']       = os.environ.get('MAIL_APP_PASSWORD', '')
 app.config['MAIL_DEFAULT_SENDER'] = ('Chico Safe', os.environ.get('MAIL_USERNAME', ''))
